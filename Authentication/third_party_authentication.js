1d8ec9b0747010d33004a90b3f9bf16f365be8ab
Mfuon Leonard <mfolee@gmail.com>
2022-07-26 14:03:46 +0300

    fix functions to authenticate tp

---
+++ Authentication/third_party_authentication.js
@@ -0,0 +1,54 @@
+const AWS = require("aws-sdk");
+const cognito = new AWS.CognitoIdentityServiceProvider();
+const pool_id = process.env.POOL_ID;
+const client_id = process.env.CLIENT_ID;
+
+exports.handler = (event, context, callback) => {
+
+    if (event.newPassword !== undefined && event.password !== undefined && event.newPassword.length > 8) {
+        const paramsForSetPass = {
+            Password: event.newPassword, UserPoolId: pool_id, Username: event.email, Permanent: true
+        };
+        cognito.initiateAuth(params(event.email, event.password), function (err, data) {
+            if (err) {
+                callback(null, successResponse('Failed',err));
+            } else {
+                cognito.adminSetUserPassword(paramsForSetPass, (err, d) => {
+                    if (err) {
+                        callback(null, successResponse('Failed',err));
+                    } else {
+                        callback(null,successResponse('Password reset successful', null));
+                    }
+                });
+            }
+        });
+    } else {
+        cognito.initiateAuth(params(event.email, event.password), function (err, data) {
+            if (err) {
+                callback(null, successResponse('Failed',err));
+            } else {
+                const response = {
+                    AccessToken: data.AuthenticationResult.IdToken,
+                    ExpiresIn: data.AuthenticationResult.ExpiresIn,
+                    TokenType: 'auth'
+                };
+                callback(null, successResponse('Successful',response));
+            }
+        });
+    }
+};
+
+const successResponse = (msg,dat) => {
+    return {
+        message: msg,
+        data: dat
+    };
+};
+
+const params = (email, password) => {
+    return {
+        AuthFlow: "USER_PASSWORD_AUTH", ClientId: client_id, AuthParameters: {
+            'USERNAME': email, 'PASSWORD': password
+        },
+    };
+};
